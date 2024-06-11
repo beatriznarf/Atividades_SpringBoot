39e68e230366b916e8202360b44d24449705fdc4
Beatriz Narciso <beatriz-narciso2@hotmail.com>
2024-06-11 00:25:47 -0300

    Atualizando a classe 'OrganizacoesDtos'

--- animes/src/main/java/bsd/dtos/OrganizacoesDtos.java
+++ animes/src/main/java/bsd/dtos/OrganizacoesDtos.java
@@ -1,4 +1,14 @@
 package bsd.dtos;
 
+
+import lombok.Data;
+
+@Data
 public class OrganizacoesDtos {
+
+    private Long id;
+    private String nome;
+    private String sigla;
+    private String chefe;
+    private int membros;
 }
